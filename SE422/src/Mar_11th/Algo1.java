50a01da5355b89f13cfd969cbe3404a2afa1eea0
MohammedHalkawt <164218243+MohammedHalkawt@users.noreply.github.com>
2025-03-11 08:13:47 +0300

    test

---
+++ SE422/src/Mar_11th/Algo1.java
@@ -0,0 +1,10 @@
+package Mar_11th;
+public class Algo1{
+    public void method1(){
+        System.out.println("Method1 is called");
+    }
+
+    public void method2(){
+        System.out.println("Method2 is called");
+    }
+}
